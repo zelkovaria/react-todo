94b2c1932405f4cb81fe63db2b0680ce7511fad4
zelkovaria <hyein396@gmail.com>
2023-12-28 22:54:07 +0900

    10.3.1 App에서 todos 상태 사용하기

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import "./App.css";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링해 보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어 보기",
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
-}
+};
 
 export default App;
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,13 +4,16 @@ import {
   MdRemoveCircleOutline,
 } from "react-icons/md";
 import "./TodoListItem.scss";
+import cn from "classnames";
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+      <div className={cn("checkbox", { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
