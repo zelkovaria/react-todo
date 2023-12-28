a86e46dc4d554e3b43bac19e6abd93775ea9514b
zelkovaria <hyein396@gmail.com>
2023-12-28 21:50:58 +0900

    10.2.1 TodoTemplate 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TodoTemplate from "./TodoTemplate";
 
 function App() {
-  return <div>TodoApp을 만들자!</div>;
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
 }
 
 export default App;
