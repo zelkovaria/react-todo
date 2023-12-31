15da7c23e1becb6b212f386ed97cdd877f6451cf
zelkovaria <hyein396@gmail.com>
2024-01-01 03:38:12 +0900

    10.3.4 수정 기능 구현

--- src/App.js
+++ src/App.js
@@ -45,10 +45,18 @@ const App = () => {
     [todos]
   );
 
+  const onToggle = useCallback((id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  });
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
