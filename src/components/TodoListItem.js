1c6a6e123a5520f480db0c280b2f190fcd46fd8b
zelkovaria <hyein396@gmail.com>
2024-01-01 22:17:47 +0900

    11.8.3 TodoListItem 수정

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,11 +7,11 @@ import {
 import "./TodoListItem.scss";
 import cn from "classnames";
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
+    <div className="TodoListItem-virtualized" style={style}>
       <div className={cn("checkbox", { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
