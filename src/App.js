d238555c17c29a15fcd1f43d7dd3edc629c478ec
zelkovaria <hyein396@gmail.com>
2024-01-01 14:30:12 +0900

    11.5.1 useState의 함수형 업데이트

--- src/App.js
+++ src/App.js
@@ -19,30 +19,24 @@ function createBulkTodos() {
 const App = () => {
   const [todos, setTodos] = useState(createBulkTodos);
 
-  const nextId = useRef(2501);
+  const nextId = useRef(4);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos]
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos]
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
   const onToggle = useCallback((id) => {
-    setTodos(
+    setTodos((todos) =>
       todos.map((todo) =>
         todo.id === id ? { ...todo, checked: !todo.checked } : todo
       )
